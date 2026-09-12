3583069eea8ad9a1205ff99aa9cf0bb23127d4aa
aryan12cmyk <apandey09319@gmail.com>
2026-09-13 03:55:48 +0530

    Add files via upload

---
+++ day 1/q1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    double a, b;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);
+    printf("Sum = %.2lf\n", a + b);
+    return 0;
+}
---
+++ day 1/q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    double a, b;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);
+    printf("Sum = %.2lf\n", a + b);
+    printf("Difference = %.2lf\n", a - b);
+    printf("Product = %.2lf\n", a * b);
+    if (b != 0)
+        printf("Quotient = %.2lf\n", a / b);
+    else
+        printf("Quotient = Undefined (division by zero)\n");
+    return 0;
+}
